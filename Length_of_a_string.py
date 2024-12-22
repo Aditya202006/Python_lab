26d1c7c736720a89afc9e85b1a510b4f25b2e629
Aditya <adityakanchana@gmail.com>
2024-12-22 10:25:06 +0530

    Length_of_a_string.py

---
+++ Length_of_a_string.py
@@ -0,0 +1,13 @@
+# write a python program to find the length of the string without using the len() method \
+
+input_string= input("Enter a string :")
+count = 0
+for i in input_string:
+   count += 1
+print("Length of the string is ",count)
+
+'''
+output:
+Enter a string :python
+Length of the string is 6
+'''
