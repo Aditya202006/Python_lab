5fa6eead5257fd99cdb8166505a797be03b9c9ba
Aditya <adityakanchana@gmail.com>
2024-12-22 10:27:47 +0530

    Vowels.py

---
+++ Vowels.py
@@ -0,0 +1,14 @@
+# write a python program to count the numberof vowels in the string
+
+n=input('Enter a String :')
+count=0
+for i in n:
+	if i in 'aeiouAEIOU':
+		count+=1
+print('Number of vowels in a string is :',count)
+
+'''
+output:
+Enter a String :python
+Number of vowels in a string is : 1
+'''
