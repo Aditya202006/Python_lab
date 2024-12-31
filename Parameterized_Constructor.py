ed262f960d3fe46089e1f4fcbe6199265cf1b7e3
Aditya <adityakanchana@gmail.com>
2024-12-31 18:46:19 +0530

    Parameterized_Constructor.py

---
+++ Parameterized_Constructor.py
@@ -0,0 +1,17 @@
+class add:
+	def __init__(self,f,s):
+		self.first = f
+		self.second = s
+	def display(self):
+		print("First numbner = ",self.first)
+		print("second number = ",self.second)
+		print("sum = ",self.first+self.second)
+dc = add(3,4)
+dc.display()
+
+'''
+output:
+First numbner =  3
+second number =  4
+sum =  7
+'''
