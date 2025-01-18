6b96309993070bb6b8f9571b6ee37291ed869066
KANCHANA ADITYA RAMANA SAI NAGENDRA <adityakanchana@gmail.com>
2025-01-18 16:31:40 +0530

    Maximum_of_two_using_max().py

---
+++ Maximum_of_two_using_max().py
@@ -0,0 +1,13 @@
+#  Python program that takes two integer inputs from the user and prints the maximum of the two using max().
+
+a = int(input("Enter the a value : "))
+b = int(input("Enter the b value : "))
+print("Maximum of a, b is : ", max(a, b))
+
+'''
+OUTPUT
+
+Enter the a value : 3
+Enter the b value : 7
+Maximum of a, b is :  7
+'''
